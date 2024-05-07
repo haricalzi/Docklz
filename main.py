637a80190d4bad35861184173f666f627efd68f9
haricalzi <calzihari@gmail.com>
2024-05-07 16:36:04 +0200

    main

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from gestione_directory import *
 
 #funzione che stampa il menù iniziale e chiede quale operazione si vuole effettuare
 def presentazione():
+
     os.system("clear")
     print("---------------------------------------------")
     print("--- SECURITY ANALISYS OF DOCKER CONTAINER ---")
@@ -14,7 +15,7 @@ def presentazione():
     print("\n---------------------------------------------")
     print("\n\nNB 1: potrebbe essere richiesta la password di root in alcuni passaggi, in quanto alcuni comandi necessitano di sudo per essere eseguiti\n")
     print("NB 2: si presuppone che sul sistema sia già stato installato configurato correttamente Docker")
-    print("\n\nInserire l'opztrivy fs --scanners vuln,secret,misconfig . ione desiderata: ")
+    print("\n\nInserire l'opzione desiderata: ")
 
 
 #funzione che gestice la scelta effettuata dall'utente
@@ -45,9 +46,18 @@ def assegna_compito(s):
 #inizio main vero e proprio ----------------------------------------------------------
 
 #funzione che stampa il menù iniziale e chiede quale operazione si vuole effettuare
-presentazione()
-#scelta iniziale da fare
-s = int(input())
-assegna_compito(s)
+end = "false"
+while (end != "true"):
+    #stampo il menù di benvenuto 
+    presentazione()
+    #scelta iniziale da fare
+    assegna_compito(int(input()))
+    print("\n\n\n\n Vuoi effettuare un'altra operazione?")
+    if (int(input() == 0)):
+        end = "true"
 
+#pulisco e termino
+os.system("clear")
+print("Grazie per aver utilizzato questo tool, spero ti sia stato utile")
+print("Per ulteriori informazione: Hari / github / linkedin")
 #fine main vero e proprio ------------------------------------------------------------
