6f68f8c40b068b28af4282b9ce56525ec4baffa3
haricalzi <calzihari@gmail.com>
2024-05-27 16:55:47 +0200

    add variables

--- docalzi/analysis.py
+++ docalzi/analysis.py
@@ -4,13 +4,13 @@ import ssvc
 cve = "CVE-2023-0013"
 anno = cve[4:8]
 
-
+req = f"https://github.com/trickest/cve/blob/main/{anno}/{cve}.md"
 
 decision = ssvc.Decision(
-    exploitation='poc',
-    automatable='no',
-    technical_impact='total',
-    mission_wellbeing='high',
+    exploitation='poc',         #none, poc, active
+    automatable='no',           #yes, no
+    technical_impact='total',   #partial, total
+    mission_wellbeing='high',   #low, medium, high
 )
 
 outcome = decision.evaluate()
