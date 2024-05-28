79ad3ba4d54ca7d1703a65d86601799421ce4a7e
haricalzi <calzihari@gmail.com>
2024-05-28 14:23:46 +0200

    fix

--- docalzi/analysis.py
+++ docalzi/analysis.py
@@ -9,11 +9,11 @@ url = f"https://github.com/trickest/cve/blob/main/{anno}/{cve}.md"
 
 browser = Selenium()
 
-try:
-
-    options = {
+options = {
         "arguments": ["--headless"]
     }
+
+try:
 
     browser.open_available_browser(url, options=options)
 
@@ -22,26 +22,24 @@ try:
     search_text1 = "No PoCs found on GitHub currently"
     search_text2 = "No PoCs from references"
     page_source = browser.get_source()
+
     if search_text1 in page_source and search_text2 in page_source:
-        exploitation='none'
-        print(exploitation)
+        exploitable='none'
     else:
-        exploitation='poc'
-        print(exploitation)
+        exploitable='poc'
 except Exception as e:
     print(f"Si è verificato un errore: {e}")
 finally:
-    # Chiudi il browser
     browser.close_all_browsers()
 
 
 decision = ssvc.Decision(
-    exploitation='poc',         #none, poc, active
-    automatable='no',           #yes, no
-    technical_impact='total',   #partial, total
-    mission_wellbeing='high',   #low, medium, high
+    exploitation=exploitable,   # none, poc, (active) --> from trickest on github
+    automatable='no',           # yes, no
+    technical_impact='total',   # partial, total
+    mission_wellbeing='high',   # low, medium, high
 )
 
 outcome = decision.evaluate()
-
-print(outcome.action)
+outcome_cutted = str(outcome.action)[11:]
+print(outcome_cutted)
