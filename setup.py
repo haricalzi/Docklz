a8602a3148f675d62d9af2b6cc63eba92bd1e8f4
haricalzi <calzihari@gmail.com>
2024-06-06 17:00:21 +0200

    python version requirements

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='docklz',
     version='1.0.0',
+    python_requires='>=3.11, <4.0',
     packages=find_packages(),
     install_requires=[
         'argparse', 
