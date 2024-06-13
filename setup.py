16eaa7b97194e7976c882a5e9412254c47984243
haricalzi <calzihari@gmail.com>
2024-06-13 18:46:13 +0200

    add info

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='docklz',
     version='1.0.0',
+    description='Strumento che effettua analisi di sicurezza relative a immagini e container Docker in modo automatizzato',
+    author='Hari Calzi',
+    author_email='calzihari@gmail.com',
+    download_url='https://github.com/haricalzi/Docklz',
     python_requires='>=3.11',
     packages=find_packages(),
     install_requires=[
